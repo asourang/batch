34b623dbf1c781faebb0b32f728ce4f0a71a2a88
asourang <42590201+asourang@users.noreply.github.com>
2020-12-30 04:09:07 -0500

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Image, ScrollView, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>welcome to my hybrid mobile app , it works both ios and android </Text>
+     <ScrollView>
+
+    <Text style={{ fontSize: 96}}>Chicken Paprikash Ingredient</Text>
+    <Text style={{ fontSize: 40 }}>1 (12 ounce) package egg noodles </Text>
+    <Text style={{ fontSize: 40}}>2 tablespoons butter </Text>
+    <Text style={{ fontSize: 40 }}>1 tablespoon minced parsley </Text>
+    <Text style={{ fontSize: 40 }}>1 teaspoon ground black pepper, divided </Text>
+    <Text style={{ fontSize: 40 }}>3 shallots, thinly sliced </Text>
+    <Text style={{ fontSize: 40 }}>6 cloves garlic, coarsely chopped</Text>
+    <Text style={{ fontSize: 40 }}>2 cups arrabbiata pasta sauce</Text>
+    <Text style={{ fontSize: 40 }}>¼ cup chicken broth</Text>
+    <Text style={{ fontSize: 40 }}>3 tablespoons red wine vinegar </Text>
+    <Text style={{ fontSize: 40 }}>2 pounds boneless, skinless chicken thighs </Text>
+    <Text style={{ fontSize: 40 }}>1 cup plain yogurt, divided </Text>
+    <Text style={{ fontSize: 40 }}>3 tablespoons paprika</Text>
+    <Text style={{ fontSize: 40 }}>1 tablespoon olive oil </Text>
+    <Text style={{ fontSize: 40 }}></Text>
+    
+  </ScrollView> 
+      
       <StatusBar style="auto" />
+      
     </View>
   );
 }
